40bb407f2756597433142085e059827a75dcca54
ewasion137 <formanban94@gmail.com>
2026-05-14 09:24:38 +0300

    Update main.lua

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,1 +1,19 @@
+-- scripts/main.lua
+
+-- Global table for HyprWin core communication
+HyprWin = {}
+
+-- Dispatcher called from C++
+HyprWin.dispatch_event = function(event_type, hwnd, title)
+    -- EVENT_OBJECT_SHOW = 0x8002
+    if event_type == 0x8002 then
+        log("Window Opened: [" .. title .. "] HWND: " .. hwnd)
+    end
+    
+    -- EVENT_OBJECT_HIDE = 0x8003
+    if event_type == 0x8003 then
+        log("Window Closed/Hidden: HWND: " .. hwnd)
+    end
+end
+
 log("HyprWin Lua logic started from scripts/main.lua")
