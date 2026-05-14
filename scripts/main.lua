4f4e9a0b39f664c4847b4c02cfd27d72abc8e1d5
ewasion137 <formanban94@gmail.com>
2026-05-14 09:32:02 +0300

    Changing lua file

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,19 +1,23 @@
 -- scripts/main.lua
 
--- Global table for HyprWin core communication
 HyprWin = {}
 
--- Dispatcher called from C++
+-- Basic event dispatcher
 HyprWin.dispatch_event = function(event_type, hwnd, title)
-    -- EVENT_OBJECT_SHOW = 0x8002
+    -- EVENT_OBJECT_SHOW (Window Created)
     if event_type == 0x8002 then
-        log("Window Opened: [" .. title .. "] HWND: " .. hwnd)
+        log("Tiling new window: " .. title)
+        
+        -- Get primary monitor resolution
+        local sw, sh = wm.get_screen_size()
+        
+        -- Simple Tile: Put new window on the left half
+        wm.move_window(hwnd, 0, 0, sw / 2, sh)
     end
     
-    -- EVENT_OBJECT_HIDE = 0x8003
     if event_type == 0x8003 then
-        log("Window Closed/Hidden: HWND: " .. hwnd)
+        log("Window removed from layout: " .. hwnd)
     end
 end
 
-log("HyprWin Lua logic started from scripts/main.lua")
+log("HyprWin: Tiling engine ready.")
